4c4ad498ac416b0c470cedaf3fcf9109ae30d145
zczqxc5 <146037962+zczqxc5@users.noreply.github.com>
2024-04-22 20:51:41 +0100

    landing page

---
+++ landing_page/documentation/assets/js/main.js
@@ -0,0 +1,27 @@
+(function() {
+
+  'use strict';
+
+ 
+
+  // smooth scroll
+  $("a").on("click", function(event) {
+
+      if (this.hash !== "") {
+          event.preventDefault();
+
+          var hash = this.hash;
+
+          $("html, body").animate({
+
+              scrollTop: $(hash).offset().top - 50
+
+          }, 850);
+
+      }
+
+  });
+
+  
+
+})();
