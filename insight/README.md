440fc63bd400776031263baf0d11785ba9d3f2ea
SHEAU-JWU WU <146037423+sjosk@users.noreply.github.com>
2024-04-26 16:17:23 +0100

    Debug apk release

--- insight/README.md
+++ insight/README.md
@@ -1,4 +1,4 @@
-# insight
+# insightn
 
 A new Flutter project.
 
---
+++ insight/android/app/src/main/kotlin/com/example/insightn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.insightn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
